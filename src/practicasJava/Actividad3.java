99b6b1eeb24cddf56f94196f574c0d1574ef8bd7
Nicole Olivares <nicoleolivaresv@gmail.com>
2022-08-24 11:45:12 -0400

    actividad 3

---
+++ src/practicasJava/Actividad3.java
@@ -0,0 +1,21 @@
+package practicasJava;
+
+public class Actividad3 {
+
+	public static void main(String[] args) {
+		// mismo ejercicio de la actividad 2 con un bucle for. 
+		//Muestra los números del 1 al 100 (ambos incluidos)
+		
+		//ciclo for
+		//declaro la variable int i = 1;
+		//indico la condición i <= 100;
+		//incremento i++
+		//imprimo 
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
+
+		
+	}
+
+}
